e8b1d54387979e1c75932ff3fb31b7003bb3d5da
Lorena <lore.arp.hogar@outlook.com>
2025-11-28 15:41:41 -0500

    Added Sundar favicon but didnt show on the browser.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,6 +3,7 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
+<link rel="icon" href="/Sundar_favicon%20.png" type="image/png">
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
