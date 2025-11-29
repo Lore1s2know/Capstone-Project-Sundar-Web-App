d3a95eb0570e95d7c6e2dd19449bc86c47057802
Lorena <lore.arp.hogar@outlook.com>
2025-11-29 12:38:25 -0500

    added reviews link to sidebar

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,6 +14,7 @@
             <flux:navlist variant="outline">
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="chat-bubble-left-right" :href="route('reviews.index')" :current="request()->routeIs('reviews.index')" wire:navigate>{{ __('Community Reviews') }}</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
